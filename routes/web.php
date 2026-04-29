034c6fc6f2376a7c07c6ce17eee2698bb15f99f7
Bondarenko22091 <bondarenko12318@yandex.com>
2026-04-29 10:50:59 +0700

    Добавлен веб-интерфейс на Blade

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BugReportController;
 
-// Страница с формой
 Route::get('/issue/create', function () {
     return view('bug-report');
 });
 
-// Обработка формы
 Route::post('/issue/store', [BugReportController::class, 'store'])->name('issues.store');
 
