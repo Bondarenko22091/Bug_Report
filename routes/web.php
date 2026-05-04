eb8080f1d5a06b7c8930069f56c64f961856dabc
Bondarenko22091 <bondarenko12318@yandex.com>
2026-05-04 18:21:34 +0700

    777

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BugReportController;
 
+Route::get('/', function () {
+    return redirect('/issue/create');
+});
+
 Route::get('/issue/create', function () {
     return view('bug-report');
 });
 
 Route::post('/issue/store', [BugReportController::class, 'store'])->name('issues.store');
-
